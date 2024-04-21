f60c5872fad268af1dc04936adeaaf82c1458d24
AM2101322 <114479943+AM2101322@users.noreply.github.com>
2024-04-21 22:08:32 +0200

    just the user atntication and key thing left

--- mainapp.cpp
+++ mainapp.cpp
@@ -10,26 +10,35 @@
 const char key[] = "123";
 int portNum;
 char ip[16];
+int choice;
 
 int main() {
-    std::cout << "Enter the IP address you want to host on:" << std::endl;
-    std::cin.getline(ip, sizeof(ip));
+    std::cout << "Welcome to the walkie-talkie app" << std::endl << "What would you like to do?" << std::endl << "1: Create a server" << std::endl << "2: Connect to a server" << std::endl;
+    std::cin >> choice;
 
-    std::cout << "On what port:" << std::endl;
-    std::cin >> portNum;
+    std::cin.ignore();  // Ignore newline character left in the input buffer
 
-    // Start the server in a thread
-    std::thread server_thread(create_server, portNum, ip);
-    
-    // allow some time for the server to start up
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // This is not always necessary but can help in avoiding immediate connection issues.
+    if (choice == 1) {
+        // If the user wants to host
+        std::cout << "Enter the IP address you want to host on:" << std::endl;
+        std::cin.getline(ip, sizeof(ip));
 
-    // Start the client in thread
-    std::thread client_thread(createClient, portNum, ip);
+        std::cout << "On what port:" << std::endl;
+        std::cin >> portNum;
+        std::cout << "All set now, run the client." << std::endl;
+        create_server(portNum, ip);
+    } else if (choice == 2) {
+        // If the user wants to connect
+        std::cout << "Enter the IP address you want to connect to:" << std::endl;
+        std::cin.getline(ip, sizeof(ip));
 
-    // Join both threads
-    server_thread.join();
-    client_thread.join();
+        std::cout << "What port are you talking on:" << std::endl;
+        std::cin >> portNum;
 
+        createClient(portNum, ip);
+    } else {
+        std::cout << "Invalid choice, try again." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    }
     return 0;
 }
